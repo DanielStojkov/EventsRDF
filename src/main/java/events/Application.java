320e9d78f0450182bc59c19c9487a31ef4de5b3b
Daniel <stojkov.daniel.93@gmail.com>
2016-08-04 19:19:13 +0200

    enable scheduling

--- src/main/java/events/Application.java
+++ src/main/java/events/Application.java
@@ -3,11 +3,13 @@ package events;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Daniel on 8/4/2016.
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
