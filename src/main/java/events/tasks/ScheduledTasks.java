81f4bc2317daae36ee2485a918bd3ab65cde18e6
Goce Cvetanov <Goce.Cvetanov@netcetera.com>
2016-08-12 22:05:19 +0200

    Add parser and storage service to scheduled task

--- src/main/java/events/tasks/ScheduledTasks.java
+++ src/main/java/events/tasks/ScheduledTasks.java
@@ -1,16 +1,30 @@
 package events.tasks;
 
+import events.models.CulturalEvent;
+import events.parsers.HtmlParser;
+import events.service.EventStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * A component that runs on a schedule.
  */
 @Component
 public class ScheduledTasks {
+    private static final String EVENTS_FILE = "events.txt";
 
-    @Scheduled(cron = "0 0 8 * * *")
+    @Autowired
+    private HtmlParser parser;
+
+    @Autowired
+    private EventStorageService storageService;
+
+    @Scheduled(cron = "0 0 0 * * *")
     public void example() {
-
+        List<CulturalEvent> events = parser.parse();
+        storageService.addEventsToFile(EVENTS_FILE, events);
     }
 }
